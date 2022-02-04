b10f49508cfc8374ec8c1aac0ceb58069bb99460
Higor-Gabriel <higorgabriel.fdiniz@gmail.com>
2022-02-04 16:30:28 -0300

    Exercicio de jogo de adivinhacao da alura
    Importando arquivos dentro de outro arquivo

---
+++ Exemplos alura/Forca.py
@@ -0,0 +1,11 @@
+
+def jogar_forca():
+    print("*********************************")
+    print("Bem vindo ao jogo de Forca")
+    print("*********************************")
+
+
+    print("*** FIM DO JOGO ***")
+
+if __name__ == "__main__":
+    jogar_forca()
