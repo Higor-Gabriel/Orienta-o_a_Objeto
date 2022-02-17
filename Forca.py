2c5168d43397d224f2a6d870e135f2543a33a64f
Higor-Gabriel <higorgabriel.fdiniz@gmail.com>
2022-02-17 11:53:20 -0300

    Exercicio de jogo de adivinhacao da alura
    Continuando a desenvolver o jogo de Forca

--- Forca.py
+++ Forca.py
@@ -1,10 +1,22 @@
+import random
 
 def jogar_forca():
+
     print("*********************************")
     print("Bem vindo ao jogo de Forca")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_certas = ["_" for letra in palavra_secreta]
 
     enforcou = False
