5d380b41e41eae6ebd5da93fa8c4bef81baf7469
Higor-Gabriel <higorgabriel.fdiniz@gmail.com>
2022-02-17 09:11:14 -0300

    Exercicio de jogo de adivinhacao da alura
    Continuando a desenvolver o jogo de Forca

--- Exemplos alura/Forca.py
+++ Exemplos alura/Forca.py
@@ -4,22 +4,39 @@ def jogar_forca():
     print("Bem vindo ao jogo de Forca")
     print("*********************************")
 
-    palavra_secretea = "banana"
+    palavra_secreta = "maça".upper()
+    letras_certas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_certas)
 
     while not enforcou and not acertou:
 
-        chute = input("Qual a letra ?")
-        index = 0
-        for letra in palavra_secretea:
-            if chute == letra:
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_certas[index] = letra
+                index += 1
+        else:
+            erros +=1
 
-        print("Jogando...")
+        enforcou = erros == 6
+        acertou = "_" not in letras_certas
+        print(letras_certas)
+
+    if acertou:
+        print("Você ganhou o jogo, Parabéns!")
+    else:
+        print("Você perdeu!")
     print("*** FIM DO JOGO ***")
 
 if __name__ == "__main__":
+
+
     jogar_forca()
